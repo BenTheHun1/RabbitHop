9607f72e633ee0ebd9462b285ad7d2bdbbb0ef54
Benjamin Hunsicker <benhunsicker1@gmail.com>
2025-04-03 14:25:12 -0400

    Improving framerate

--- HyperProto/Assets/Scripts/GameManager.cs
+++ HyperProto/Assets/Scripts/GameManager.cs
@@ -31,6 +31,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+		Application.targetFrameRate = 60;
         pause.SetActive(false);
         dead.SetActive(false);
         win.SetActive(false);
